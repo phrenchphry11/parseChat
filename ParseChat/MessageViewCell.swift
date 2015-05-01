0f3f20194b7af55554aadc735d4c551fcb5fea8d
Holly French <holly.french@rd.io>
2015-05-01 20:41:19 +0200

    first commit

---
+++ ParseChat/MessageViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MessageViewCell.swift
+//  ParseChat
+//
+//  Created by Holly French on 5/1/15.
+//  Copyright (c) 2015 Holly French. All rights reserved.
+//
+
+import UIKit
+
+class MessageViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
